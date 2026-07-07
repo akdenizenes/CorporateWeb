6bed971a7995ec6e70983b8d4e6716500f004b39
Enes Akdeniz <enesakdeniz@posta.mu.edu.tr>
2026-07-07 12:20:21 +0300

    feat: integrate Match-3 game into portfolio modal

--- CorporateWeb.WebUI/wwwroot/games/match-3/flutter_bootstrap.js
+++ CorporateWeb.WebUI/wwwroot/games/match-3/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1683665596"
+    serviceWorkerVersion: "2449812900"
   }
 });
